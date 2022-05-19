ed51bc555701f0ad3b5bed5c9f394301706e7a0b
Khaja Nazmuddin Shaik <iamkhaja0786@gmail.com>
2022-05-19 14:15:00 +0530

    ProductName

--- SeamsterProducts/Models/Common/OrderCosmoDBActivity.cs
+++ SeamsterProducts/Models/Common/OrderCosmoDBActivity.cs
@@ -11,7 +11,7 @@ namespace Seamster_Products.Models.Order
     {
 
 
-        // The Azure Cosmos DB endpoint for running this sample.
+        // The Azure Cosmos DB endpoint for running this sample EndPoint.
         private static readonly string EndpointUri = "https://localhost:8081";
         // The primary key for the Azure Cosmos account.
         private static readonly string PrimaryKey = "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
